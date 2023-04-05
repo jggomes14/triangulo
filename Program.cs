88ef240ebd3a2c54e4839d3a380c8d1c901acf32
jggomes14 <jggomes141097@gmail.com>
2023-04-05 17:17:00 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace triangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vamos construir um triangulo?");
+            Triangulo triangulo = new Triangulo();
+            Console.WriteLine("Insira o valor do primeiro lado");
+            triangulo.ladox = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Insira o valor do segundo lado");
+            triangulo.ladoy = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Insira o valor do terceiro lado");
+            triangulo.ladoz = Convert.ToDouble(Console.ReadLine());
+
+            if (triangulo.TrianguloValido())
+            {
+                if (triangulo.Equilatero())
+                {
+                    Console.WriteLine("O triângulo é equilátero.");
+                }
+                else if (triangulo.Isosceles())
+                {
+                    Console.WriteLine("O triângulo é isósceles.");
+                }
+                else if (triangulo.Escaleno())
+                {
+                    Console.WriteLine("O triângulo é escaleno.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Os valores inseridos não formam um triângulo.");
+            }
+        }
+    }
+}
---
+++ Triangulo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace triangulo
+{
+    internal class Triangulo
+    {
+        public double ladox;
+        public double ladoy;
+        public double ladoz;
+
+        public bool TrianguloValido()
+        {
+            return (ladox + ladoy > ladoz) && (ladox + ladoz > ladoy) && (ladoy + ladoz > ladox);
+        }
+
+        public bool Equilatero()
+        {
+            return TrianguloValido() && (ladox == ladoy && ladoy == ladoz);
+        }
+
+        public bool Isosceles()
+        {
+            return TrianguloValido() && (ladox == ladoy || ladox == ladoz || ladoy == ladoz);
+        }
+
+        public bool Escaleno()
+        {
+            return TrianguloValido() && !Equilatero() && !Isosceles();
+        }
+    }
+
+}
